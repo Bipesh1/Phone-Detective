866f46bea339f8abd200fe5481e6731f33ecfab2
Aman-0707 <aman7kc@gmail.com>
2026-02-25 20:47:32 +0545

    update versionCode to 13 and versionName to 1.1.3 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.phonedetective.phone_detective"
         minSdk = flutter.minSdkVersion // or flutter.minSdkVersion
         targetSdk = 35 // or flutter.targetSdkVersion
-        versionCode = 12 // or flutter.versionCode
-        versionName = "1.1.2" // or flutter.versionName
+        versionCode = 13 // or flutter.versionCode
+        versionName = "1.1.3" // or flutter.versionName
     }
 
     compileOptions {
