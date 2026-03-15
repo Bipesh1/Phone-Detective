c0976bcae580a7bf9e2f3db0d7aa0a21cb19dcde
Aman-0707 <aman7kc@gmail.com>
2026-03-15 20:39:04 +0545

    update versionCode to 17 and versionName to 1.1.7 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.phonedetective.phone_detective"
         minSdk = flutter.minSdkVersion // or flutter.minSdkVersion
         targetSdk = 35 // or flutter.targetSdkVersion
-        versionCode = 14 // or flutter.versionCode
-        versionName = "1.1.4" // or flutter.versionName
+        versionCode = 17 // or flutter.versionCode
+        versionName = "1.1.7" // or flutter.versionName
     }
 
     compileOptions {
